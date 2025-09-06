8212e93a283f795afb3c019abb43e7e26d4b831c
Rainier <rainiertabing21@gmail.com>
2025-09-05 22:04:39 -0400

    Rewrote the printTicket so that it declares local variable, amountLeftToPay

--- TicketMachine.java
+++ TicketMachine.java
@@ -65,7 +65,8 @@ public class TicketMachine
      */
     public void printTicket()
     {
-        if(balance <= price) {
+        int amountLeftToPay = price - balance;
+        if(amountLeftToPay <= 0) {
             // Simulate the printing of a ticket.
             System.out.println("##################");
             System.out.println("# The BlueJ Line");
@@ -80,8 +81,7 @@ public class TicketMachine
             balance = balance - price;
         }
         else {
-            System.out.printf("You must insert at least %d more cents.%n",
-                              price - balance);
+            System.out.printf("You must insert at least " + amountLeftToPay + " more cents.");
         }
     }
 
